5c1f6346e6b111733f76beb49e239de288f99727
LS <luissolorzano88@gmail.com>
2026-06-09 20:12:04 -0400

    Delete src/components/GlobalSearch.jsx

--- src/components/GlobalSearch.jsx
+++
@@ -1,4 +0,0 @@
-import { useState, useMemo, useEffect, useRef } from 'react';
-import { TRAYECTO_COLORS, TRAYECTO_BG } from '../constants';
-import { parseClase } from '../utils/parsing';
-// estilos inline (S) se pueden mantener aquí o mover a un objeto compartido
